80a12af62f9a264390bc8055687204f1280dbdda
Florian Samson <floriansamson@gmail.com>
2014-02-27 10:11:33 +0100

    affiche liste promo bdd

--- index.php
+++ index.php
@@ -4,10 +4,28 @@
         <title>Base de données</title>
     </head>
     <body>
-        <h1><center>Se connecter</center></h1>            
+        <h1><center>Liste des promotions</center></h1>            
         <?php
-        $bdd = new PDO('mysql:host=localhost;dbname=iia', 'root', '');
-
+        
+        $dsn = 'mysql:dbname=iia;host=127.0.0.1';
+        $dbUser = 'root';
+        $dbPass = '';        
+        $oDb = new PDO($dsn, $dbUser, $dbPass);
+        
+        $oResultat = $oDb->query('SELECT * FROM promotion ORDER BY pro_nom ASC');
+        
+//        $promotion = $oResultat->fetch();
+//        //var_dump($promotion);
+//        echo '<p>', $promotion['pro_nom'], '</p>';
+//        $promotion = $oResultat->fetch();
+//        echo $promotion[1];
+        
+        while($promotion = $oResultat->fetch())
+        {
+            echo '<p>', $promotion['pro_nom'], '</p>';
+        }
+        
+        
         ?>
         
     </body>
