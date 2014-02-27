c6e5dc4c0ca466a2ea788ae24f6ed60dea3c82e4
Florian Samson <floriansamson@gmail.com>
2014-02-27 09:38:06 +0100

    initialisation du dépot

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Base de données</title>
+    </head>
+    <body>
+        <h1><center>Se connecter</center></h1>            
+        <?php
+        $bdd = new PDO('mysql:host=localhost;dbname=iia', 'root', '');
+
+        ?>
+        
+    </body>
+</html>
